b793ea72a0815b8d0cd1f27f29742bbebae4d47e
liucun <a123>
2018-06-10 19:57:49 +0800

    修改

---
+++ app/src/main/java/com/android/androidwolf/Test.java
@@ -0,0 +1,8 @@
+package com.android.androidwolf;
+
+/**
+ * Created by liucun on 2018/6/10.
+ */
+
+public class Test {
+}
--- app/src/main/java/com/android/androidwolf/abase/MainActivity.java
+++ app/src/main/java/com/android/androidwolf/abase/MainActivity.java
@@ -1,7 +1,10 @@
-package com.android.androidwolf;
+package com.android.androidwolf.abase;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.android.androidwolf.R;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
