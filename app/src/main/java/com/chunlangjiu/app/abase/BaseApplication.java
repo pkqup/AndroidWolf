d8e0236cc4c01ea06bd94cc722624d0fb4380511
wuqugame <wuqugame@anzogame.com>
2018-08-30 09:59:06 +0800

    添加代码

--- app/src/main/java/com/chunlangjiu/app/abase/BaseApplication.java
+++ app/src/main/java/com/chunlangjiu/app/abase/BaseApplication.java
@@ -49,7 +49,9 @@ public class BaseApplication extends MultiDexApplication {
 
 
     public static void initToken() {
-        //44fd86649bd5d33eb2038f01349e397ab9c56d1b4985285b889a92ffdf2e81ca
+        //个人:  44fd86649bd5d33eb2038f01349e397ab9c56d1b4985285b889a92ffdf2e81ca
+        //商家： 1ed0c5b256e840e0dbfeae01c86eac1918c7f4532875d76012ac3e8e4238abcf
+        SPUtils.put("token","1ed0c5b256e840e0dbfeae01c86eac1918c7f4532875d76012ac3e8e4238abcf");
         token = (String) SPUtils.get("token", "");
         KLog.e("-----token-----", token);
     }
